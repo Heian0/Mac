d3344bd6d5d70e8c88601b113f1a786fdb6f6265
Heian0 <77514857+Heian0@users.noreply.github.com>
2022-05-26 18:12:14 -0700

    1st Commit.
    Three Activities active: Dashboard, Mac Calc, New Item.
    Mac Calc is functional, while Dashboard and New Item are not.
    
    Main goal for next Commit: Implement the New Item functionality and
    database for items.

---
+++ app/src/main/java/com/example/bigmac/Dashboard.java
@@ -0,0 +1,37 @@
+package com.example.bigmac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Dashboard extends AppCompatActivity {
+
+    private ActivityHandler activityHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        Button btnMacCalc = findViewById(R.id.btnMacCalc);
+        btnMacCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                GoToMacCalc();
+            }
+        });
+    }
+
+
+    private void GoToMacCalc()
+    {
+        Intent intent = new Intent(this, MacCalc.class);
+        System.out.println(this);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/bigmac/MacCalc.java
@@ -0,0 +1,132 @@
+package com.example.bigmac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Spinner;
+import android.text.TextUtils;
+
+public class MacCalc extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
+    private Calculator calculator = new Calculator();
+    private User user = new User();
+    private ActivityHandler activityHandler = new ActivityHandler();
+
+    private Button btnGetMCal;
+    private Button btnNewItem;
+
+    private int activityIndex = 3;
+
+    public static Context macCalcContext;
+
+    /*
+    private Toolbar toolbarMenu;
+
+    private TabLayout tabLayoutMenu;
+    private ViewPager viewPagerMenu;
+
+    private NewUserFrag newUserFrag;
+    private NewItemFrag newItemFrag;
+    private MacCalcFrag macCalcFrag;
+    private TrackerFrag trackerFrag;
+    private DailyReportFrag dailyReportFrag;
+
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mac_calc);
+
+        Button btnNewItem = findViewById(R.id.btnNewItem);
+        btnNewItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                GoToNewItem();
+            }
+        });
+
+
+        Spinner spinnerGender = findViewById(R.id.spinnerGender);
+        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this, R.array.sArrayGenders, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGender.setAdapter((genderAdapter));
+        spinnerGender.setOnItemSelectedListener(this);
+
+        Spinner spinnerActivity = findViewById(R.id.spinnerActivity);
+        ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(this, R.array.sArrayActivity, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerActivity.setAdapter((activityAdapter));
+        spinnerActivity.setOnItemSelectedListener(this);
+    }
+
+    private void GoToNewItem()
+    {
+        Intent intent = new Intent(this, NewItem.class);
+        startActivity(intent);
+    }
+
+    public void OnBtnGetMCalClick(View view)
+    {
+        EditText edtTxtAge = findViewById(R.id.edtTxtAge);
+        EditText edtTxtHeight = findViewById(R.id.edtTxtHeight);
+        EditText edtTxtWeight = findViewById(R.id.edtTxtWeight);
+
+        if (    TextUtils.isDigitsOnly(edtTxtAge.getText().toString())
+                && TextUtils.isDigitsOnly(edtTxtHeight.getText().toString())
+                && TextUtils.isDigitsOnly(edtTxtWeight.getText().toString())
+                && (user.getGender() == 0 || user.getGender() == 1)
+        )
+        {
+            int age = Integer.valueOf(edtTxtAge.getText().toString());
+            int height = Integer.valueOf(edtTxtHeight.getText().toString());
+            int weight = Integer.valueOf(edtTxtWeight.getText().toString());
+
+            int mCal = calculator.CalculateMCal(age, height, weight, user.getGender(), user.getActivity());
+            TextView txtMCal = findViewById(R.id.txtMCal);
+            txtMCal.setText(Integer.toString(mCal));
+        }
+
+        else
+        {
+            return;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        switch (parent.getId())
+        {
+            case R.id.spinnerGender:
+                if (position == 0) user.setGender(0);
+                else user.setGender(1);;
+                break;
+
+            case R.id.spinnerActivity:
+                switch(position)
+                {
+                    case 0: user.setActivity(0); break;
+                    case 1: user.setActivity(1); break;
+                    case 2: user.setActivity(2); break;
+                    case 3: user.setActivity(3); break;
+                    case 4: user.setActivity(4); break;
+                }
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
+    }
+}
